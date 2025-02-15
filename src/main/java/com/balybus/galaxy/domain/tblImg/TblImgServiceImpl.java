dc5fd1a9e62b0d05702790a73f0ded786e86a31a
jg <ella.code.j@gmail.com>
2025-02-15 19:11:10 +0900

    chore: 파일 업로드 API - swagger 관련 지정 및 return 타입 변경

--- src/main/java/com/balybus/galaxy/domain/tblImg/TblImgServiceImpl.java
+++ src/main/java/com/balybus/galaxy/domain/tblImg/TblImgServiceImpl.java
@@ -1,9 +1,8 @@
 package com.balybus.galaxy.domain.tblImg;
 
 import com.balybus.galaxy.domain.tblImg.dto.ImgRequestDto;
+import com.balybus.galaxy.global.exception.BadRequestException;
 import com.balybus.galaxy.global.exception.ExceptionCode;
-import com.balybus.galaxy.global.response.ApiResponse;
-import com.balybus.galaxy.global.response.SuccessCode;
 import com.balybus.galaxy.global.utils.FileUploadUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -20,18 +19,20 @@ public class TblImgServiceImpl {
     private final FileUploadUtils fileUploadUtils;
 
     @Transactional
-    public ApiResponse<?> uploadImg(UserDetails userDetails, ImgRequestDto.UploadImg dto) {
+    public List<Long> uploadImg(UserDetails userDetails, ImgRequestDto.UploadImg dto) {
         List<Long> imgSeq = new ArrayList<>();
         MultipartFile[] photoFiles = dto.getPhotoFiles();
         if (photoFiles != null && photoFiles.length > 0) {
             try {
                 imgSeq = fileUploadUtils.uploadFiles(photoFiles);
-            } catch (Exception e) {
-                return ApiResponse.ERROR(ExceptionCode.INTERNAL_SEVER_ERROR);
+            } catch (BadRequestException e) {
+                throw e;
+            }  catch (Exception e) {
+                throw new BadRequestException(ExceptionCode.INTERNAL_SEVER_ERROR);
             }
         }
 
-        return ApiResponse.SUCCESS(SuccessCode.UPLOAD_SUCCESS, imgSeq);
+        return imgSeq;
     }
 
 }
