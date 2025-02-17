1c3265890108c2a9b1f05282fab70deca15e5049
kimjunhee <manuna530@gmail.com>
2025-02-17 21:10:51 +0900

    FIX / center 문제 수정

--- src/main/java/com/balybus/galaxy/center/repository/TblSingUpCenterRepository.java
+++ src/main/java/com/balybus/galaxy/center/repository/TblSingUpCenterRepository.java
@@ -1,9 +1,9 @@
-//package com.balybus.galaxy.center.repository;
-//
-//import com.balybus.galaxy.center.domain.TblSingUpCenter;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import java.util.Optional;
-//
-//public interface TblSingUpCenterRepository extends JpaRepository<TblSingUpCenter, Long> {
-//    Optional<TblSingUpCenter> findByCenterName(String centerName);
-//}
+package com.balybus.galaxy.center.repository;
+
+import com.balybus.galaxy.center.domain.TblSingUpCenter;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface TblSingUpCenterRepository extends JpaRepository<TblSingUpCenter, Long> {
+    Optional<TblSingUpCenter> findByCenterName(String centerName);
+}
