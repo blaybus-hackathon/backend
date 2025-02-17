f98f7f3d1dc9e34881fa76c50891e95534e8efef
kimjunhee <manuna530@gmail.com>
2025-02-17 20:40:09 +0900

    FIX / patient 병합 에러 수정

--- src/main/java/com/balybus/galaxy/patient/domain/TblPatientadress.java
+++ src/main/java/com/balybus/galaxy/patient/domain/TblPatientadress.java
@@ -1,34 +1,34 @@
-//package com.balybus.galaxy.patient.domain;
-//
-//import com.balybus.galaxy.domain.BaseEntity;
-//import jakarta.persistence.Column;
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.Id;
-//import lombok.*;
-//import org.hibernate.annotations.Comment;
-//import org.springframework.stereotype.Component;
-//
-//@Entity
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Builder
-//@Getter
-//@Setter
-//
-//public class TblPatientadress extends BaseEntity {
-//
-//    @Column(name = "af_seq")
-//    @Comment("시.도 구분자")
-//    private String tblPatientFirst;
-//
-//    @Column(name = "as_seq")
-//    @Comment("시.군.구 구분자")
-//    private String tblPatientSecond;
-//
-//    @Column(name = "at_seq")
-//    @Comment("읍.면.동 구분자")
-//    private String tblPatientThrid;
-//
-//
-//
-//}
+package com.balybus.galaxy.patient.domain;
+
+import com.balybus.galaxy.domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.Comment;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+public class TblPatientadress extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "patient_addr_seq")
+    @Comment("어르신 주소 구분자")
+    private Long id;
+
+    @Column(name = "af_seq")
+    @Comment("시.도 구분자")
+    private String tblPatientFirst;
+
+    @Column(name = "as_seq")
+    @Comment("시.군.구 구분자")
+    private String tblPatientSecond;
+
+    @Column(name = "at_seq")
+    @Comment("읍.면.동 구분자")
+    private String tblPatientThrid;
+
+}
--- src/main/java/com/balybus/galaxy/patient/repository/TblPatientRepository.java
+++ src/main/java/com/balybus/galaxy/patient/repository/TblPatientRepository.java
@@ -1,11 +1,13 @@
-//package com.balybus.galaxy.patient.repository;
-//
-//import com.balybus.galaxy.patient.domain.TblPatient;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import java.util.Optional;
-//
-//public interface TblPatientRepository extends JpaRepository<TblPatient, Long> {
-//
-//    Optional<TblPatient> findByName(String name);
-//}
+package com.balybus.galaxy.patient.repository;
+
+import com.balybus.galaxy.patient.domain.TblPatient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TblPatientRepository extends JpaRepository<TblPatient, Long> {
+
+    Optional<TblPatient> findByName(String name);
+}
