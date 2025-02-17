867cdc6c8640b544e9cd17297f6eddf4d8f58bb4
jg <ella.code.j@gmail.com>
2025-02-18 03:23:54 +0900

    feat: TblCare(어르신 케어 항목) entity/repository 생성

---
+++ src/main/java/com/balybus/galaxy/domain/tblCare/TblCareRepository.java
@@ -0,0 +1,6 @@
+package com.balybus.galaxy.domain.tblCare;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TblCareRepository extends JpaRepository<TblCare, Long> {
+}
