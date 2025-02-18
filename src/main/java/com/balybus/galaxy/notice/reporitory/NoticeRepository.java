82db404361c318028ded14e50b8b4aa62c587a02
금예빈 <147026802+bebeaubn@users.noreply.github.com>
2025-02-18 11:36:59 +0900

    Delete src/main/java/com/balybus/galaxy/notice directory

--- src/main/java/com/balybus/galaxy/notice/reporitory/NoticeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.balybus.galaxy.notice.reporitory;
-
-import com.balybus.galaxy.patient.domain.TblPatient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NoticeRepository extends JpaRepository<TblPatient, Long> {
-}
-
