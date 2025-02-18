15cd901bd9246a147dbb57e89c48e728e37fb7eb
kimjunhee <manuna530@gmail.com>
2025-02-18 14:30:45 +0900

    FIX / 컬럼명 수정

--- src/main/java/com/balybus/galaxy/helper/domain/TblHelper.java
+++ src/main/java/com/balybus/galaxy/helper/domain/TblHelper.java
@@ -46,30 +46,31 @@ public class TblHelper extends BaseEntity implements ChangeProfileImg {
     @Comment("요양보호사 연락처")
     private String phone;
 
-    @Column(name = "helper_age")
+    @Column(name = "helper_gender")
     @Comment("요양보호사 성별")
     private Integer gender;
 
     @Column(name = "helper_birthday")
+    @Comment("요양보호사 생년월일")
     private String birthday;
 
     @Column(name = "helper_address_detail", length = 255)
     @Comment("상세 주소")
     private String addressDetail;
 
-    @Column(name = "helper_essential_cert_no", length = 11, nullable = false)
+    @Column(name = "helper_essential_cert_no", length = 11)
     @Comment("요양 보호사 자격증 번호")
     private String essentialCertNo;
 
-    @Column(name = "helper_care_cert_no", length = 11, nullable = false)
+    @Column(name = "helper_care_cert_no", length = 11)
     @Comment("간병사 자격증 번호")
     private String careCertNo;
 
-    @Column(name = "helper_nurse_cert_no", length = 11, nullable = false)
+    @Column(name = "helper_nurse_cert_no", length = 11)
     @Comment("병원 동행 매니저 자격증 번호")
     private String nurseCertNo;
 
-    @Column(name = "helper_post_partum_cert_no", length = 11, nullable = false)
+    @Column(name = "helper_post_partum_cert_no", length = 11)
     @Comment("산후 관리사 자격증 번호")
     private String postPartumCertNo;
 
@@ -97,14 +98,14 @@ public class TblHelper extends BaseEntity implements ChangeProfileImg {
     @Comment("급여 협의 가능 여부")
     private Boolean wageNegotiation;
 
-    @Column(name = "helper_introduce")
+    @Column(name = "helper_introduce", length = 255)
     @Comment("요양 보호사 자기 소개")
     private String introduce;
 
-    @Column(name = "helper_strength")
-    @Comment("요양 보호사 강점")
-    @Convert(converter = StringListConverter.class)
-    private List<String> strengths;
+//    @Column(name = "helper_strength")
+//    @Comment("요양 보호사 강점")
+//    @Convert(converter = StringListConverter.class)
+//    private List<String> strengths;
 
     @Column(name = "helper_exp")
     @Comment("간병경력")
