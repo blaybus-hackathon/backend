ee3680d698a92a7b7fcfe345de48906552ea475d
금예빈 <147026802+bebeaubn@users.noreply.github.com>
2025-02-19 18:58:09 +0900

    수정후 DTO파일 생성한거 다시 올림
    DTO파일 최근 수정일자로 다시 업로드 25.02.19일

---
+++ src/main/java/com/balybus/galaxy/patient/dto/response/TblPatientTimeLogResponse.java
@@ -0,0 +1,16 @@
+package com.balybus.galaxy.patient.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import java.time.LocalTime;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class TblPatientTimeLogResponse {
+    private Long id;
+    private int pt_date;
+    private LocalTime pt_start_time;
+    private LocalTime pt_end_time;
+}
