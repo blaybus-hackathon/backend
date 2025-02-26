f975b05c1f9b79366e283347ebdf04abac8d5fce
kimjunhee <manuna530@gmail.com>
2025-02-27 07:11:52 +0900

    새 커밋 메시지

--- src/main/java/com/balybus/galaxy/domain/tblImg/TblImgServiceImpl.java
+++ src/main/java/com/balybus/galaxy/domain/tblImg/TblImgServiceImpl.java
@@ -1,20 +1,30 @@
 package com.balybus.galaxy.domain.tblImg;
 
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.balybus.galaxy.global.exception.BadRequestException;
 import com.balybus.galaxy.global.exception.ExceptionCode;
 import com.balybus.galaxy.global.utils.file.FileUploadUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class TblImgServiceImpl {
     private final FileUploadUtils fileUploadUtils;
+    private final AmazonS3Client amazonS3;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
 
     @Transactional
     public List<Long> uploadImg(MultipartFile[] photoFiles) {
@@ -30,6 +40,30 @@ public class TblImgServiceImpl {
         }
 
         return imgSeq;
+    }
+
+    /**
+     * S3에 이미지 업로드 하기
+     */
+    public String uploadImageToS3(MultipartFile image) throws IOException {
+        String fileName = UUID.randomUUID() + "_" + image.getOriginalFilename(); // 고유한 파일 이름 생성
+
+        // 메타데이터 설정
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentType(image.getContentType());
+        metadata.setContentLength(image.getSize());
+
+        // S3에 파일 업로드 요청 생성
+        PutObjectRequest putObjectRequest = new PutObjectRequest(bucket, fileName, image.getInputStream(), metadata);
+
+        // S3에 파일 업로드
+        amazonS3.putObject(putObjectRequest);
+
+        return getPublicUrl(fileName);
+    }
+
+    private String getPublicUrl(String fileName) {
+        return String.format("https://%s.s3.%s.amazonaws.com/%s", bucket, amazonS3.getRegionName(), fileName);
     }
 
 }
--- src/main/java/com/balybus/galaxy/global/temp/TempController.java
+++ src/main/java/com/balybus/galaxy/global/temp/TempController.java
@@ -2,6 +2,7 @@ package com.balybus.galaxy.global.temp;
 
 import com.balybus.galaxy.domain.tblImg.TblImgServiceImpl;
 import com.balybus.galaxy.domain.tblImg.dto.ImgRequestDto;
+import com.balybus.galaxy.global.exception.BadRequestException;
 import com.balybus.galaxy.global.exception.ErrorResponse;
 import com.balybus.galaxy.member.dto.response.MemberResponse;
 import io.swagger.v3.oas.annotations.Operation;
@@ -14,6 +15,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+import static com.balybus.galaxy.global.exception.ExceptionCode.UPLOAD_FAILED;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,7 +33,7 @@ public class TempController {
         return "Authenticated user: " + userDetails.getUsername();
     }
 
-    @PostMapping("/upload-img")
+//    @PostMapping("/upload-img")
     @Operation(summary = "파일 업로드 API",
             description = "사용자가 이미지를 업로드하고 서버에 저장하는 기능을 제공합니다. " +
                     "이미지 파일을 multipart/form-data로 전송해야 하며, 성공적으로 업로드되면 이미지 구분자(imgSeq) 리스트가 반환됩니다.")
@@ -43,6 +49,28 @@ public class TempController {
                                        ImgRequestDto.UploadImg dto) {
         return ResponseEntity.ok().body(imgService.uploadImg(dto.getPhotoFiles()));
     }
+
+    @PostMapping("/upload-img")
+    @Operation(summary = "파일 업로드 API",
+            description = "사용자가 이미지를 업로드하고 S3에 저장하는 기능을 제공합니다. " +
+                    "이미지 파일을 multipart/form-data로 전송해야 하며, 성공적으로 업로드되면 이미지 구분자(imgSeq) 리스트가 반환됩니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "파일 저장 성공",
+                    content = @Content(schema = @Schema(implementation = MemberResponse.SignInDto.class))),
+            @ApiResponse(responseCode = "5000", description = "예상하지 못한 서버 에러",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "6000", description = "사용자정의에러코드:파일 업로드 실패",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
+    public ResponseEntity<?> uploadImg(@RequestParam("file") MultipartFile file) {
+        try {
+            String imgUrl = imgService.uploadImageToS3(file);
+            return ResponseEntity.ok(imgUrl);
+        } catch (IOException e) {
+            throw new BadRequestException(UPLOAD_FAILED);
+        }
+    }
+
 
     @PostMapping("/authentication-mail")
     public ResponseEntity<?> authenticationMail(@AuthenticationPrincipal UserDetails userDetails) {
