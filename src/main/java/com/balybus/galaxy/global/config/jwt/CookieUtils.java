da026817b36158ede71c2263dc049988439f1104
jg <ella.code.j@gmail.com>
2025-02-27 14:02:42 +0900

    build: 운영환경에 따른 쿠키 설정 변경

--- src/main/java/com/balybus/galaxy/global/config/jwt/CookieUtils.java
+++ src/main/java/com/balybus/galaxy/global/config/jwt/CookieUtils.java
@@ -21,6 +21,8 @@ import java.util.Optional;
 public class CookieUtils {
     @Value("${jwt.front-domain}")
     private String frontDomain;
+    @Value("${spring.profiles.active}")
+    private String active;
 
     public Optional<Cookie> getCookie(HttpServletRequest request, String name) {
         Cookie[] cookies = request.getCookies();
@@ -36,20 +38,26 @@ public class CookieUtils {
 
     public void saveCookie(HttpServletResponse response, String newAccessToken) {
         // JWT 토큰을 HttpOnly 쿠키로 설정
-//        ResponseCookie jwtCookie = ResponseCookie.from("JWT-TOKEN", newAccessToken)
-//                .httpOnly(true)     // 클라이언트에서 쿠키를 읽을 수 없도록 설정
-//                .secure(true)       // HTTPS에서만 전송하도록 설정 (개발 환경에서는 false)
-//                .sameSite("Strict") // SameSite 설정을 통해 CSRF 공격 방어
-//                .path("/")          // 쿠키 경로 설정
-//                .maxAge(Duration.ofDays(7)) // 쿠키 유효 기간 설정 (예: 7일)
+        ResponseCookie jwtCookie;
+        log.info("spring.profiles.active::"+active);
+        if(active.equals("dev")){
+            jwtCookie = ResponseCookie.from("JWT-TOKEN", newAccessToken)
+                    .httpOnly(true)     // 클라이언트에서 쿠키를 읽을 수 없도록 설정
+                    .secure(true)       // HTTPS에서만 전송하도록 설정 (개발 환경에서는 false)
+                    .sameSite("Strict") // SameSite 설정을 통해 CSRF 공격 방어
+                    .path("/")          // 쿠키 경로 설정
+                    .maxAge(Duration.ofDays(7)) // 쿠키 유효 기간 설정 (예: 7일)
 //                .domain(frontDomain)
-//                .build();
-        ResponseCookie jwtCookie = ResponseCookie.from("JWT-TOKEN", newAccessToken)
-                .httpOnly(true)     // 클라이언트에서 쿠키를 읽을 수 없도록 설정
-                .secure(false)       // HTTPS에서만 전송하도록 설정 (개발 환경에서는 false)
-                .path("/")          // 쿠키 경로 설정
-                .maxAge(Duration.ofDays(7)) // 쿠키 유효 기간 설정 (예: 7일)
-                .build();
+                    .build();
+        } else {
+            jwtCookie = ResponseCookie.from("JWT-TOKEN", newAccessToken)
+                    .httpOnly(true)     // 클라이언트에서 쿠키를 읽을 수 없도록 설정
+                    .secure(false)       // HTTPS에서만 전송하도록 설정 (개발 환경에서는 false)
+                    .path("/")          // 쿠키 경로 설정
+                    .maxAge(Duration.ofDays(7)) // 쿠키 유효 기간 설정 (예: 7일)
+                    .build();
+        }
+
 
         // JWT 쿠키를 응답 헤더에 추가
         response.addHeader(HttpHeaders.SET_COOKIE, jwtCookie.toString());
