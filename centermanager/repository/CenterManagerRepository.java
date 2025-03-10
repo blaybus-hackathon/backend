6375be71bd68ebd4f547804b24d875181b32bab8
금예빈 <147026802+bebeaubn@users.noreply.github.com>
2025-03-10 16:47:41 +0900

    Delete centermanager directory

--- centermanager/repository/CenterManagerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.balybus.galaxy.centermanager.repository;
-
-
-import com.balybus.galaxy.centermanager.entity.CenterManagerEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CenterManagerRepository extends JpaRepository<CenterManagerEntity, Long> {
-}
