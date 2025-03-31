9c1987fafa633a3dc1cbc19d89f7d1b08be30c39
jg <ella.code.j@gmail.com>
2025-03-31 14:55:45 +0900

    [CHORE] 사용하지 않는 import 제거

--- src/main/java/com/balybus/galaxy/address/repository/TblAddressThirdRepository.java
+++ src/main/java/com/balybus/galaxy/address/repository/TblAddressThirdRepository.java
@@ -1,6 +1,5 @@
 package com.balybus.galaxy.address.repository;
 
-import com.balybus.galaxy.address.domain.TblAddressSecond;
 import com.balybus.galaxy.address.domain.TblAddressThird;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
