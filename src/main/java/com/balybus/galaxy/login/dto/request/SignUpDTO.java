486aa7524e59d1f3d3fc1c89dcd7973eea8c760e
kimjunhee <manuna530@gmail.com>
2025-05-08 04:26:00 +0900

    feature / helper-search 데이터 타입 수정

--- src/main/java/com/balybus/galaxy/login/dto/request/SignUpDTO.java
+++ src/main/java/com/balybus/galaxy/login/dto/request/SignUpDTO.java
@@ -10,7 +10,7 @@ import lombok.ToString;
 @Setter
 @NoArgsConstructor
 @ToString
-public class SignUpDTO {
+public class    SignUpDTO {
     private RoleType roleType;
     private String email;
     private String password;
