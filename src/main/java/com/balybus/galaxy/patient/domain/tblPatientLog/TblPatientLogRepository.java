b70c45f86b742ac3a6f5c25c72e49a476ac626b3
kimjunhee <mike@peakffice.com>
2025-05-26 19:44:38 +0900

    feature / Jpql 적용

--- src/main/java/com/balybus/galaxy/patient/domain/tblPatientLog/TblPatientLogRepository.java
+++ src/main/java/com/balybus/galaxy/patient/domain/tblPatientLog/TblPatientLogRepository.java
@@ -3,6 +3,8 @@ package com.balybus.galaxy.patient.domain.tblPatientLog;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,4 +13,7 @@ public interface TblPatientLogRepository extends JpaRepository<TblPatientLog, Lo
     Page<TblPatientLog> findByManagerId(Long managerSeq, Pageable page);
     Page<TblPatientLog> findByPatientIdAndManagerId(Long patientSeq, Long managerSeq, Pageable page);
     List<TblPatientLog> findAllByManagerId(Long managerSeq);
+
+    @Query("SELECT pl FROM TblPatientLog pl JOIN FETCH pl.patient WHERE pl.manager.id = :managerId")
+    List<TblPatientLog> findAllByManagerIdWithPatient(@Param("managerId") Long managerId);
 }
