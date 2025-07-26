38261409c0aafaff3aa950dd7839c4b2f63c0a88
jg <ella.code.j@gmail.com>
2025-07-26 21:27:22 +0900

    [FEATURE] 요양보호사 - 어르신 공고 상세 조회 API

--- src/main/java/com/balybus/galaxy/patient/recruit/dto/RecruitResponseDto.java
+++ src/main/java/com/balybus/galaxy/patient/recruit/dto/RecruitResponseDto.java
@@ -102,63 +102,6 @@ public class RecruitResponseDto {
 
     }
 
-    @Data
-    @EqualsAndHashCode(callSuper = true)
-    public static class GetOneRecruitPatientInfoToHelper extends PatientBaseDto {
-        private Boolean linkingYn;          //어르신 기본정보로 업데이트 할 것인지 여부
-        private Boolean wageNegotiation;    //급여 협의 여부
-        private int wageState;              // 1:시급, 2:일급, 3:주급 구분
-        private int wage;                   // 급여
-        private String requestContents;     // 기타 요청 사항
-
-        private CareChoiceListDto careChoice;
-        public GetOneRecruitPatientInfoToHelper(TblPatientLog patient, List<TblPatientTimeLog> patientTimeList){
-            this.wageNegotiation = patient.getWageNegotiation();
-            this.wageState = patient.getWageState();
-            this.wage = patient.getWage();
-            this.requestContents = patient.getRequestContents();
-            this.welfare = patient.getWelfare();                // 복리후생
-
-            this.afSeq = patient.getTblAddressFirst().getId();      // 광역시.도 구분자
-            this.asSeq = patient.getTblAddressSecond().getId();     // 시.군.구 구분자
-            this.atSeq = patient.getTblAddressThird().getId();      // 읍.면.동 구분자
-            this.careLevel = patient.getCareLevel();                // 장기요양등급
-            this.inmateState = patient.getInmateState();            // 동거인 여부
-            this.workType = patient.getWorkType();                  // 근무종류
-            this.gender = patient.getGender();                      // 남성:1 여성:2
-            this.dementiaSymptom = patient.getDementiaSymptom();    // 치매증상
-            this.serviceMeal = patient.getServiceMeal();            // 식사보조
-            this.serviceToilet = patient.getServiceToilet();        // 배변보조
-            this.serviceMobility = patient.getServiceMobility();    // 이동보조
-            this.serviceDaily = patient.getServiceDaily();          // 일상생활
-            this.name = patient.getName();                          // 어르신 이름
-            this.birthDate = patient.getBirthDate();                // 생년월일
-            this.weight = patient.getWeight();                      // 몸무게
-            this.diseases = patient.getDiseases();                  // 보유 질병/질환
-            this.timeNegotiation = patient.getTimeNegotiation();    // 돌봄 요일 시간 협의 여부
-
-            List<SavePatientTimeInfo> timeList = new ArrayList<>();
-            for(TblPatientTimeLog patientTime: patientTimeList){
-                timeList.add(new SavePatientTimeInfo(patientTime));
-            }
-            this.timeList = timeList; // 돌봄요일 리스트 : 선택한 요일에 대한 데이터만 리스트에 넣어서 전달.
-        }
-
-        public void setCareBaseDtoNull(){
-            this.careLevel = null;        // 장기요양등급
-            this.inmateState = null;      // 동거인여부
-            this.workType = null;         // 근무종류
-            this.gender = null;           // 남성:1 여성:2
-            this.dementiaSymptom = null;  // 치매증상
-            this.serviceMeal = null;      // 식사보조
-            this.serviceToilet = null;    // 배변보조
-            this.serviceMobility = null;  // 이동보조
-            this.serviceDaily = null;     // 일상생활
-            this.welfare = null;          // 복리후생
-        }
-
-    }
-
     @Getter
     @Builder
     public static class UpdateRecruitPatientInfo{
